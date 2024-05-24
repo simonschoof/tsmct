b929e183a1f59d8d25e5d582b3c3eeb16ab9601e
Simon Schoof <47699713+simonschoof@users.noreply.github.com>
2024-05-24 10:29:17 +0200

    #NOREF | upgrade to kotlin 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,8 @@ plugins {
 	id("com.adarshr.test-logger") version "4.0.0"
 	id("org.flywaydb.flyway") version "10.13.0"
 	id("com.github.ben-manes.versions") version "0.51.0"
-	kotlin("jvm") version "1.9.23"
-	kotlin("plugin.spring") version "1.9.23"
+	kotlin("jvm") version "2.0.0"
+	kotlin("plugin.spring") version "2.0.0"
 }
 
 group = "com.simonschoof"
