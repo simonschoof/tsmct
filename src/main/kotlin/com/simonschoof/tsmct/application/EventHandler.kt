e526e1868854f023b221945ad4d01bc76304ceda
Simon Schoof <47699713+simonschoof@users.noreply.github.com>
2024-06-14 12:14:05 +0200

    #C6 | delete result class, add event handler

--- src/main/kotlin/com/simonschoof/tsmct/application/EventHandler.kt
+++ src/main/kotlin/com/simonschoof/tsmct/application/EventHandler.kt
@@ -1,6 +1,7 @@
 package com.simonschoof.tsmct.application
 
 import com.simonschoof.tsmct.domain.InventoryItemCreated
+import com.simonschoof.tsmct.domain.InventoryItemNameChanged
 import com.trendyol.kediatr.NotificationHandler
 import org.springframework.stereotype.Component
 
@@ -8,5 +9,12 @@ import org.springframework.stereotype.Component
 class InventoryItemCreatedEventHandler : NotificationHandler<InventoryItemCreated> {
     override suspend fun handle(notification: InventoryItemCreated) {
         println("created a new inventory item with name ${notification.name}")
+    }
+}
+
+@Component
+class InventoryItemNameChangedEventHandler : NotificationHandler<InventoryItemNameChanged> {
+    override suspend fun handle(notification: InventoryItemNameChanged) {
+        println("changed name of inventory item to name ${notification.newName}")
     }
 }
