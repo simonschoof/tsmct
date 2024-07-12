c4456ef277c8758093cbc9e6bdaf0ec7e28a3e7f
Simon Schoof <47699713+simonschoof@users.noreply.github.com>
2024-07-12 23:56:33 +0200

    #C6 | create docker file for wasmJs UI, add to compose

--- cqrs-es-ui/composeApp/src/wasmJsMain/kotlin/HttpStuff.wasmJs.kt
+++ cqrs-es-ui/composeApp/src/wasmJsMain/kotlin/HttpStuff.wasmJs.kt
@@ -1,8 +1,5 @@
 import io.ktor.client.HttpClient
-import io.ktor.client.call.body
 import io.ktor.client.engine.js.Js
-import io.ktor.client.request.get
-import io.ktor.client.statement.HttpResponse
 
 actual val httpClient: HttpClient
     get() = HttpClient(Js)
